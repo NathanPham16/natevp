4f51113189581a415f3baecf6db0dd28156d3915
natepham <natepham16@gmail.com>
2021-01-12 23:48:17 -0800

    Launch Website

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-70px";
+  }
+}
